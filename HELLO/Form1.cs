5778a0d049f944ebc8ef7f37dca5d77cb507a0ff
Luke Steven John Case <rc0032733@student.riversidecollege.ac.uk>
2018-11-20 09:59:51 +0000

    wellcome to program

--- HELLO/Form1.cs
+++ HELLO/Form1.cs
@@ -19,7 +19,7 @@ namespace HELLO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello " + txtBxname.Text + " and " + txtbxname2.Text);
+            MessageBox.Show("Welcome to " + txtBxname.Text + "'s program" );
         }
     }
 }
